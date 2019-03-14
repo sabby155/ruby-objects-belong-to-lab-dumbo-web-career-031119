3a25437e2024a2b80f0c66235da07a4142066857
Sabrina Chowdhury <nebula155@gmail.com>
2019-03-14 17:16:49 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Song
+  attr_accessor :title
+  def initialize(title)
+    @title = title
+  end 
+  
+  
+end 
+
+
+class Artist
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+  end
+  
+end  
+
+
+class Post
+  
+  
+end 
+
+class Author
+  
+  
+end 
